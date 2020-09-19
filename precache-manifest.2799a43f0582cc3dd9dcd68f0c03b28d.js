8d690b0fa6f91703ec55c237b9568c36d6fc90bb
chankruze <chankruze@gmail.com>
2020-09-19 20:50:08 +0530

    Initial Prod Release
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ precache-manifest.2799a43f0582cc3dd9dcd68f0c03b28d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8b0b966cb75c0f79d244010c8b5e75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "793f7f961e0550a79857",
+    "url": "/static/css/main.441e22f8.chunk.css"
+  },
+  {
+    "revision": "78d4052e1302aa215ed1",
+    "url": "/static/js/2.f365af3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f365af3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "793f7f961e0550a79857",
+    "url": "/static/js/main.2c108667.chunk.js"
+  },
+  {
+    "revision": "3cd9c4de8d359528cb97",
+    "url": "/static/js/runtime-main.73439bb7.js"
+  }
+]);
