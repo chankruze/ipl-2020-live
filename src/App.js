352b80f1d483f554101c15ae793b4ab4a0074019
chankruze <chankruze@gmail.com>
2020-09-19 20:44:11 +0530

    Initial Commit
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import VideoPlayer from './components/VideoPlayer';
+
+export const STREAM_URL = 'https://ssh101.com/securelive/index.php?id=cric7dvv'
+export const STREAM_TITLE = 'CSK vs MI'
+
+function App() {
+  return (
+    <div className="App container">
+      <Navbar/>
+      <VideoPlayer streamData={{ url: STREAM_URL, title: STREAM_TITLE }} />
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,20 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Sat Sep 19 2020 19:24:21 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+import React from 'react'
+
+function VideoPlayer(props) {
+    const {url, title} = props.streamData
+
+    return (
+        <React.Fragment>
+            <iframe src={url} title={title} className="stream-player" />
+        </React.Fragment>
+    )
+}
+
+export default VideoPlayer
